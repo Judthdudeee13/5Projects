53e4f23a2c3861e928bbbde6ee1f8fd3e9e985e2
Judthdudeee13 <Judthdudeee13@gmail.com>
2026-01-16 10:09:33 -0500

    save

--- space shooter/code/main.py
+++ space shooter/code/main.py
@@ -17,21 +17,23 @@ x = 100
 
 # imports
 player_surface = pygame.image.load(join('space shooter', 'images', 'player.png')).convert_alpha()
-player_rect = player_surface.get_frect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
-player_direction = 1
+player_rect = player_surface.get_rect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
+player_direction = pygame.math.Vector2(1, 1)
+player_speed = 400
 
 star_surface = pygame.image.load(join("space shooter", 'images', 'star.png')).convert_alpha()
 star_postitions = [(randint(0, WINDOW_WIDTH), randint(0,WINDOW_HEIGHT)) for i in range(20)]
 
 meteor_surface = pygame.image.load(join("space shooter", 'images', 'meteor.png')).convert_alpha()
-meteor_rect = meteor_surface.get_frect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
+meteor_rect = meteor_surface.get_rect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
 
 laser_surface = pygame.image.load(join("space shooter", 'images', 'laser.png')).convert_alpha()
-laser_rect = laser_surface.get_frect(bottomleft = (20, WINDOW_HEIGHT-20))
+laser_rect = laser_surface.get_rect(bottomleft = (20, WINDOW_HEIGHT-20))
 
 
 
 while run:
+    dt = clock.tick() / 1000
     #eventloop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -45,9 +47,11 @@ while run:
     window.blit(laser_surface, laser_rect)
 
     #player movment
-    player_rect.x += player_direction *0.4
-    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
-        player_direction *= -1
+    if player_rect.right >= WINDOW_WIDTH or player_rect.left <= 0:
+        player_direction.x *= -1
+    if player_rect.bottom >= WINDOW_HEIGHT or player_rect.top <= 0:
+        player_direction.y *= -1
+    player_rect.center += player_direction * player_speed * dt
     window.blit(player_surface, player_rect)
 
     pygame.display.flip()
