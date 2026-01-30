dfca0435d3d1367b91ca95a5805c1a041eaa8463
Judthdudeee13 <Judthdudeee13@gmail.com>
2026-01-30 10:25:09 -0500

    class

--- space shooter/code/main.py
+++ space shooter/code/main.py
@@ -1,6 +1,13 @@
+#video 2:00:00
 import pygame
 from os.path import join
 from random import randint
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image =  pygame.image.load(join('space shooter', 'images', 'player.png')).convert_alpha()
+        self.rect = self.image.get_frect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
 
 #general setup
 pygame.init()
@@ -15,11 +22,13 @@ surface = pygame.Surface((100, 200))
 surface.fill("orange")
 x = 100
 
+player = Player()
+
 # imports
-player_surface = pygame.image.load(join('space shooter', 'images', 'player.png')).convert_alpha()
-player_rect = player_surface.get_frect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
-player_direction = pygame.math.Vector2()
-player_speed = 300
+# player_surface = pygame.image.load(join('space shooter', 'images', 'player.png')).convert_alpha()
+# player_rect = player_surface.get_frect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
+# player_direction = pygame.math.Vector2()
+# player_speed = 300
 
 star_surface = pygame.image.load(join("space shooter", 'images', 'star.png')).convert_alpha()
 star_postitions = [(randint(0, WINDOW_WIDTH), randint(0,WINDOW_HEIGHT)) for i in range(20)]
@@ -40,11 +49,15 @@ while run:
             run = False
 
     #input
-    keys = pygame.key.get_pressed()
-    player_direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
-    player_direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
+    # keys = pygame.key.get_pressed()
+    # player_direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
+    # player_direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
+    # player_direction = player_direction.normalize() if player_direction else player_direction
+    # player_rect.center += player_direction * player_speed * dt
 
-    player_rect.center += player_direction * player_speed * dt
+    recent_keys = pygame.key.get_just_pressed()
+    if recent_keys[pygame.K_SPACE]:
+        print("Fire Laser")
 
 
     #draw the game
@@ -54,7 +67,8 @@ while run:
 
     window.blit(meteor_surface, meteor_rect)
     window.blit(laser_surface, laser_rect)
-    window.blit(player_surface, player_rect)
+    #window.blit(player_surface, player_rect)
+
 
     pygame.display.flip()
 
