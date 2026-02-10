6621c96770e257ae634c160858243c93f9419814
Judthdudeee13 <Judthdudeee13@gmail.com>
2026-02-10 10:25:21 -0500

    save

--- Vampire survivor/code/player.py
+++ Vampire survivor/code/player.py
@@ -14,6 +14,7 @@ class Player(pygame.sprite.Sprite):
         keys = pygame.key.get_pressed()
         self.direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
         self.direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
+        self.direction = self.direction.normalize() if self.direction else self.direction
 
     def move(self, dt):
         self.rect.center += self.direction * self.speed * dt
