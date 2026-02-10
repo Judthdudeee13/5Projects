f48aca56c6006ac471b74c4a8bb9115d36127774
judthdudeee13 <judthdudeee13@gamil.com>
2026-02-09 22:22:05 -0500

    starting

--- Vampire survivor/code/main.py
+++ Vampire survivor/code/main.py
@@ -1,1 +1,40 @@
 from settings import *
+from player import Player
+
+class Game:
+    def __init__(self):
+        #setup
+        pygame.init()
+        self.window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Survivor')
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+        #groups
+        self.all_sprites = pygame.sprite.Group()
+
+        # sprites
+        self.player = Player((400, 300), self.all_sprites)
+
+    def run(self):
+        while self.running:
+            # dt
+            dt = self.clock.tick() / 1000
+
+            #event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            #update
+            self.window.fill('black')
+            self.all_sprites.update(dt)
+                    
+            #draw
+            self.all_sprites.draw(self.window)
+            pygame.display.flip()
+        pygame.quit()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
