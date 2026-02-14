0d3cc3dbb876a57b434bdc07c7ef6e6580966aaf
judthdudeee13 <judthdudeee13@gamil.com>
2026-02-13 19:55:33 -0500

    working on bug fix

--- Vampire survivor/code/assets.py
+++ Vampire survivor/code/assets.py
@@ -5,15 +5,19 @@ class AssetManager:
         self.images = {}
 
     def load_images(self, folder_path, entity_name):
+        frames = {}
         self.images[entity_name] = {}
-        self.frames = {'left': [], 'right': [], 'up': [], 'down': []}
-        for state in self.frames.keys():
+        for folder_path, sub_folders, file_name in walk(folder_path):
+            frames[sub_folders] = {}
+            print(frames)
+        for state in frames.keys():
             for folder_path, sub_folders, file_names in walk(join(folder_path, state)):
                 if file_names:
                     for file_name in sorted(file_names, key= lambda name: int(name.split('.')[0])):
                         full_path = join(folder_path, file_name)
                         surf = pygame.image.load(full_path).convert_alpha()
-                        self.frames[state].append(surf)
+                        frames[state].append(surf)
+        print(self.images)
 
     def get(self, entity):
         return self.images[entity]
