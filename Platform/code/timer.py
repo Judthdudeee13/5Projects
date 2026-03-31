2b0b45fecc6fe3663c1a79bd41609814b46552ea
Judthdudeee13 <judthdudeee13@gmail.com>
2026-03-31 10:21:24 -0400

    save

--- Platform/code/timer.py
+++ Platform/code/timer.py
@@ -5,6 +5,7 @@ class Timer:
         self.duration = duration
         self.start_time = 0
         self.active = False
+        self.func = func
 
     def activate(self):
         self.active = True
@@ -16,4 +17,6 @@ class Timer:
 
     def update(self):
         if pygame.time.get_ticks() - self.start_time >= self.duration:
+            if self.func and self.start_time != 0:
+                self.func()
             self.deactivate()
