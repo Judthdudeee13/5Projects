e3c6554eec54c9f78487e3c0a3e16959190e7e58
judthdudeee13 <judthdudeee13@gmail.com>
2026-04-19 21:49:43 -0400

    sace

--- Monster battle/code/main.py
+++ Monster battle/code/main.py
@@ -1,17 +1,42 @@
 from settings import *
 from support import *
 from timer import Timer
+from random import choice
+from monster import Monster, Opponent
+from ui import UI
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('Monster Battle')
         self.clock = pygame.time.Clock()
         self.running = True
+        self.import_assets()
 
         # groups 
         self.all_sprites = pygame.sprite.Group()
+
+        # data
+        player_monster_list = ['Sparchu', 'Cleaf',  'Jacana']
+        self.player_monsters = [Monster(name, self.back_surfaces[name]) for name in player_monster_list]
+        self.monster = self.player_monsters[0]
+        self.all_sprites.add(self.monster)
+        opponent_name = choice(list(MONSTER_DATA.keys()))
+        self.opponent = Opponent(opponent_name, self.front_surfs[opponent_name], self.all_sprites)
+
+        #ui
+        self.ui = UI(self.monster)
+    
+    def import_assets(self):
+        self.back_surfaces = folder_importer('Monster battle', 'images', 'back')
+        self.bg_surfs = folder_importer('Monster battle', 'images', 'other')
+        self.front_surfs = folder_importer('Monster battle', 'images', 'front')
+
+    def draw_monster_floor(self):
+        for sprite in self.all_sprites:
+            floor_rect = self.bg_surfs['floor'].get_frect(center = sprite.rect.midbottom + pygame.Vector2(0, -10))
+            self.window.blit(self.bg_surfs['floor'], floor_rect)
 
     def run(self):
         while self.running:
@@ -19,12 +44,20 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+                
+                if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_ESCAPE:
+                            self.running = False
            
             # update
             self.all_sprites.update(dt)
+            self.ui.update()
 
-            # draw  
-            self.all_sprites.draw(self.display_surface)
+            # draw 
+            self.window.blit(self.bg_surfs['bg'], (0,0))
+            self.draw_monster_floor()
+            self.all_sprites.draw(self.window)
+            self.ui.draw()
             pygame.display.update()
         
         pygame.quit()
